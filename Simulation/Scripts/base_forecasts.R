5f78f09a5c1c75307c19f27504aa701a0f9bca38
Anastasios Panagiotelis <anastasios.panagiotelis@monash.edu>
2020-03-29 11:39:13 +1100

    updated simulation scripts

--- Simulation/Scripts/base_forecasts.R
+++ Simulation/Scripts/base_forecasts.R
@@ -1,14 +1,18 @@
 #This script fits base models.
-library(tsibble)
 library(tidyverse)
-library(fable)
 library(slider)
 library(furrr)
+library(tsibble)
+library(readr)
+library(tidyr)
+library(fable)
 
 data<-read_csv('../Data/Bottom_Level_Gaussian_Simulated.csv')
 
-H<-1 #Forecast Horizon
-N<-500 #Size of window
+H<-1 # Maximum Forecast Horizon
+N<-500 # Size of window
+L<-4 # Lags to leave at beginning of window
+B<-1 # Number of sample paths for probabilistic forecasts
 
 
 #Make data windows
@@ -19,12 +23,13 @@ data_windows<-list_window<-slider::slide(data,~.x,.after=N,.complete = T)
 forecast_window <- function(data){
   data%>%pivot_longer(-Time,names_to = 'Var')%>%
     as_tsibble(key = Var,index = Time)%>%
-    model(ets=ETS(value))->m
-  p<-fabletools::generate(m,times=5,bootstrap=T)
-  return(list(mable=m,paths=p))
+    model(arima=ARIMA(value))->m
+  f<-forecast(m,h=H)
+  p<-generate(m,bootstrap=T,times=B,h=H)
+  return(list(mable=m,forecast=f,paths=p))
 }
 
 plan(multicore,workers=availableCores()-1)
-a<-map(data_windows[1:21],forecast_window)
+a<-map(data_windows[1:1000],forecast_window)
 plan(sequential)
-
+saveRDS(a,'baseforecasts.rds')
