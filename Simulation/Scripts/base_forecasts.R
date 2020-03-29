5434cc16ca695ee5531543be3dff0da8bf4aee1b
Anastasios Panagiotelis (Monash University) <anastasp@monarch-login2.erc.monash.edu>
2020-03-29 15:49:52 +1100

    fixed gitgnore and job

--- Simulation/Scripts/base_forecasts.R
+++ Simulation/Scripts/base_forecasts.R
@@ -13,7 +13,7 @@ H<-3 # Maximum Forecast Horizon
 N<-500 # Size of window
 L<-4 # Lags to leave at beginning of window
 B<-1 # Number of sample paths for probabilistic forecasts
-R<-96
+R<-9
 
 #Make data windows
 data_windows<-list_window<-slider::slide(data,~.x,.after=N+L-1,.complete = T)
@@ -26,10 +26,7 @@ forecast_window <- function(data){
   f<-forecast(m,h=H)
   p<-generate(m,bootstrap=T,times=B,h=H)
   return(list(mable=m,forecast=f,paths=p))
-  #return(NROW(data))
 }
 
-plan(multicore,workers=availableCores()-1)
 a<-map(data_windows[1:R],forecast_window)
-plan(sequential)
 saveRDS(a,'baseforecasts.rds')
--- Simulation/Scripts/job.sh
+++ Simulation/Scripts/job.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 #SBATCH --job-name=AP_BaseFC
 #SBATCH --ntasks=1
-#SBATCH --cps-per-task=1
+#SBATCH --cpus-per-task=1
 #SBATCH --export=NONE
 module load R/3.6.2-mkl
 Rscript base_forecasts.R
