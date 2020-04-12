1392728151b52d00773e64a83267d770782343b9
Anastasios Panagiotelis <anastasios.panagiotelis@monash.edu>
2020-04-12 20:31:15 +1000

    added computation of covariance matrices

--- Simulation/Base_Forecasts/README.md
+++ Simulation/Base_Forecasts/README.md
@@ -4,3 +4,5 @@
 - The file *job.sh* can be submitted onto a SLURM cluster.  This allows 16 different simulation scenarios to be run in parallel.
 - The file *simtable.R* constructs a table lining up each simulation scenario with a number from 1-16 that can be matched to the slurm task id in job.sh.
 - The file *SimulationTable.csv* is the table produces by simtable.R.
+
+Note that as well as producing base forecasts, the one-step ahead error covariance matrix is also computed at this stage (sample and shirnk). 
--- Simulation/Base_Forecasts/base_forecasts.R
+++ Simulation/Base_Forecasts/base_forecasts.R
@@ -1,4 +1,5 @@
 #This script fits base models.
+
 library(tidyverse)
 library(slider)
 library(furrr)
@@ -7,13 +8,37 @@ library(readr)
 library(tidyr)
 library(fable)
 
+#Clear workspace
+rm(list=ls())
+
+
+#Function for Shaefer Strimmer Shrinkage
+
+shrink.estim <- function(res)
+{
+  n<-nrow(res)
+  covm <- crossprod(res)/nrow(res)
+  tar <- diag(diag(covm))
+  corm <- stats::cov2cor(covm)
+  xs <- scale(res, center = FALSE, scale = sqrt(diag(covm)))
+  xs <- xs[stats::complete.cases(xs),]
+  v <- (1/(n * (n - 1))) * (crossprod(xs^2) - 1/n * (crossprod(xs))^2)
+  diag(v) <- 0
+  corapn <- stats::cov2cor(tar)
+  d <- (corm - corapn)^2
+  lambda <- sum(v)/sum(d)
+  lambda <- max(min(lambda, 1), 0)
+  W <- lambda * tar + (1 - lambda) * covm
+  return(W)
+}
+
 #Read Simulation Table
 simtable<-read_csv('SimulationTable.csv')
 
 ### Extract flags from simulation scenario
 
-#scen<-1 #If running within R uncomment this.  This will only run first scenario
-scen<-as.numeric(commandArgs()[[6]]) # If running batch job uncomment this
+scen<-1 #If running within R uncomment this.  This will only run first scenario
+#scen<-as.numeric(commandArgs()[[6]]) # If running batch job uncomment this
 
 
 simj<-simtable[scen,] #Extract row of table
@@ -26,11 +51,11 @@ modelj<-simj$model #Is model ARIMA or ETS
 data<-read_csv(paste0('../Data/',distj,'_',trendj,'.csv'))
 
 #Sample sizes
-H<-3 # Maximum Forecast Horizon
+H<-1 # Maximum Forecast Horizon
 N<-500 # Size of window
 L<-4 # Lags to leave at beginning of window
 B<-1000 # Number of sample paths for probabilistic forecasts
-R<-1000 # Number of replications
+W<-1000 # Number of windows
 
 
 #Make data windows
@@ -39,29 +64,94 @@ data_windows<-list_window<-slider::slide(data,~.x,.after=N+L-1,.complete = T)
 
 #Function for one window
 forecast_window <- function(data_w){
+  #Find start of window
+  window<-max(data_w$Time)
+  
   data_w%>%pivot_longer(-Time,names_to = 'Var')%>%
     as_tsibble(key = Var,index = Time)->d
   if(modelj=='arima'){
-    m<-model(d,arima=ARIMA(value))
+    m<-model(d,.model=ARIMA(value))
   }else{
-    m<-model(d,ets=ETS(value))
+    m<-model(d,.model=ETS(value))
   }
             
   f<-forecast(m,h=H)
+  
+  #Find Cov
+  m%>%residuals%>%
+    select(-.model)%>%
+    pivot_wider(id_cols = Time,
+                names_from = Var,
+                values_from = .resid)%>%
+    select(-Time)%>%
+    as.matrix()->E
+  
+  Sigma_sam<-t(E)%*%E/(nrow(E)-1)
+  Sigma_shr<-shrink.estim(E)
+  
   if(innovationsj=='bootstrap'){
     p<-generate(m,bootstrap=T,times=B,h=H)
   }else{
     p<-generate(m,bootstrap=F,times=B,h=H)
   }
-  return(list(mable=m,forecast=f,paths=p))
+  
+  
+  
+  return(list(mable=m%>%add_column(.window=window),
+              forecast=f%>%add_column(.window=window),
+              paths=p%>%add_column(.window=window),
+              Sigma_sam=Sigma_sam,
+              Sigma_shr=Sigma_shr))
 }
 
 #Get all results
-all<-map(data_windows[1:R],forecast_window)
+all<-map(data_windows[1:W],forecast_window)%>%
+  purrr::transpose() #Transpose to change access order in list
+
+#Isolate paths and combine to single df
+paths<-all$paths%>%
+  map_dfr(function(x){return(as_tibble(x))})
+
+#Isolate mable and combine to single df
+
+mable<-all$mable%>%
+  map_dfr(function(x){return(as_tibble(x))})
+
+#Isolate mable and combine to single df
+
+forecast<-all$forecast%>%
+  map_dfr(function(x){return(as_tibble(x))})
+
+
 
 #Save output
-saveRDS(all,paste0('../Base_Results/',
+saveRDS(paths,paste0('../Base_Results/',
                    distj,'_',
                    trendj,'_',
                    modelj,'_',
-                   innovationsj,'.rds'))
+                   innovationsj,'_paths.rds'))
+
+saveRDS(mable,paste0('../Base_Results/',
+                     distj,'_',
+                     trendj,'_',
+                     modelj,'_',
+                     innovationsj,'_mable.rds'))
+
+saveRDS(forecast,paste0('../Base_Results/',
+                     distj,'_',
+                     trendj,'_',
+                     modelj,'_',
+                     innovationsj,'_forecast.rds'))
+
+saveRDS(all$Sigma_sam,paste0('../Base_Results/',
+                        distj,'_',
+                        trendj,'_',
+                        modelj,'_',
+                        innovationsj,'_Sigma_sam.rds'))
+
+saveRDS(all$Sigma_shr,paste0('../Base_Results/',
+                        distj,'_',
+                        trendj,'_',
+                        modelj,'_',
+                        innovationsj,'_Sigma_shr.rds'))
+
