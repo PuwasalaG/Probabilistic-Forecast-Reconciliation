a93f6b6ccd120d477dfb9c81a276ccb9e7c56742
Anastasios Panagiotelis (Monash University) <anastasios.panagiotelis@monash.edu>
2020-05-10 15:02:09 +1000

    increased memory

--- Simulation/Reconcile_Forecasts/job.sh
+++ Simulation/Reconcile_Forecasts/job.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 #SBATCH --job-name=AP_RecoFC
+#SBATCH --output=array_%A_%a.out
 #SBATCH --error=array_%A_%a.err
+#SBATCH --mem-per-cpu=8192
 #SBATCH --array=1-32
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
