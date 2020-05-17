cb267a9d2bd7588d4c0549a4687ce1cd9b169a61
Anastasios Panagiotelis (Monash University) <anastasios.panagiotelis@monash.edu>
2020-05-17 10:54:30 +1000

    faster simulation

--- Simulation/Reconcile_Forecasts/job.sh
+++ Simulation/Reconcile_Forecasts/job.sh
@@ -3,8 +3,8 @@
 #SBATCH --output=job_%a.out
 #SBATCH --error=array_%a.err
 #SBATCH --mem-per-cpu=4096
-#SBATCH --time=96:00:00
-#SBATCH --array=1-32
+#SBATCH --time=24:00:00
+#SBATCH --array=1-8
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --export=NONE
