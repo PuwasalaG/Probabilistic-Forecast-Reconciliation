8caa730523a9d7adb2aae316e3691af6ff260b02
Anastasios Panagiotelis <anastasios.panagiotelis@monash.edu>
2020-05-19 10:25:21 +1000

    more tidying up of figs folder

--- Paper/Figs/README.md
+++ Paper/Figs/README.md
@@ -1,1 +1,1 @@
-The script schematics.r constructs a tikz figure depicting reconciliation.  This creates a tex file one which some postprocessing was carried out to create the figure in the pdf file.
+The script schematics.r constructs a tikz figure depicting reconciliation.  This creates a pdf file which is cropped using pdfcrop and included in the paper. 
--- Paper/Figs/schematics.r
+++ Paper/Figs/schematics.r
@@ -9,75 +9,75 @@ options(tikzLatexPackages=c(getOption("tikzLatexPackages"),"\\usepackage{amsfont
 
 ##Point forecast reconciliation.
 
-tikz('../../Presentations/CUFE2018/figs/orth_pointforerec_schematic.tex',width=4)
-plot.new()
-plot.window(xlim = c(-0.25,4),ylim = c(-0.5,5),asp=1)
-lines(c(0,0),c(-1,5))
-lines(c(-1,5),c(0,0))
-#lines(c(-1,3.5),c(-0.5,1.75),lwd=2)
-arrows(0,0,3.5,1.75,lwd=3)
-text(3.5,1.75,"{\\large ${\\bm S}$}",pos=4)
-#text(2,1,"{\\huge $\\mathfrak{s}$}",pos=1,offset = 1.3)
-#lines(c(-0.25,1),c(-1,4),lwd=2)
-#arrows(0,0,1,4,lwd=3)
-#text(1,4,"{\\Large ${\\bm R}$}",pos=3)
-
-
-
-yhat<-c(1.5,3)
-
-points(1.5,3,pch=20,cex=2,col='blue')
-text(1.5,3,"{\\large $\\color{blue}{\\hat{\\bm{y}}}$}",pos = 3,offset = 1.5)
-
-SP<-matrix(c(0.8,0.4,0.4,0.2),2,2)
-
-ytilde<-SP%*%yhat
-
-points(ytilde[1],ytilde[2],pch=20,cex=2,col='red')
-text(ytilde[1],ytilde[2],"{\\large $\\color{red}{\\tilde{\\bm{y}}}$}",pos=1,offset = 1.5)
-
-
-
-#lines(c(0,2),c(-3,5),lty=2)
-arrows(yhat[1],yhat[2],ytilde[1],ytilde[2],col='blue')
-
-
-#text(1.25,2,"{\\large ${\\color{blue} S\\circ P}$}",pos=4)
-
-points(6/7,3/7,pch=20,cex=2,col='black')
-text(6/7,3/7,"{\\large $\\color{black}{{\\bm{y}}}$}",pos=1,offset = 1.5)
-
-
-dev.off()
-
+# #tikz('../../Presentations/CUFE2018/figs/orth_pointforerec_schematic.tex',width=4)
+# plot.new()
+# plot.window(xlim = c(-0.25,4),ylim = c(-0.5,5),asp=1)
+# lines(c(0,0),c(-1,5))
+# lines(c(-1,5),c(0,0))
+# #lines(c(-1,3.5),c(-0.5,1.75),lwd=2)
+# arrows(0,0,3.5,1.75,lwd=3)
+# text(3.5,1.75,"{\\large ${\\bm S}$}",pos=4)
+# #text(2,1,"{\\huge $\\mathfrak{s}$}",pos=1,offset = 1.3)
+# #lines(c(-0.25,1),c(-1,4),lwd=2)
+# #arrows(0,0,1,4,lwd=3)
+# #text(1,4,"{\\Large ${\\bm R}$}",pos=3)
+# 
+# 
+# 
+# yhat<-c(1.5,3)
+# 
+# points(1.5,3,pch=20,cex=2,col='blue')
+# text(1.5,3,"{\\large $\\color{blue}{\\hat{\\bm{y}}}$}",pos = 3,offset = 1.5)
+# 
+# SP<-matrix(c(0.8,0.4,0.4,0.2),2,2)
+# 
+# ytilde<-SP%*%yhat
+# 
+# points(ytilde[1],ytilde[2],pch=20,cex=2,col='red')
+# text(ytilde[1],ytilde[2],"{\\large $\\color{red}{\\tilde{\\bm{y}}}$}",pos=1,offset = 1.5)
+# 
+# 
+# 
+# #lines(c(0,2),c(-3,5),lty=2)
+# arrows(yhat[1],yhat[2],ytilde[1],ytilde[2],col='blue')
+# 
+# 
+# #text(1.25,2,"{\\large ${\\color{blue} S\\circ P}$}",pos=4)
+# 
+# points(6/7,3/7,pch=20,cex=2,col='black')
+# text(6/7,3/7,"{\\large $\\color{black}{{\\bm{y}}}$}",pos=1,offset = 1.5)
+# 
+# 
+# dev.off()
+# 
 
 ##Point forecast reconciliation.
 
-tikz('pointforerec_schematic.tex',height=5)
-plot.new()
-plot.window(xlim = c(-0.25,4),ylim = c(-0.5,5))
-lines(c(0,0),c(-1,5))
-lines(c(-1,5),c(0,0))
-#lines(c(-1,3.5),c(-0.5,1.75),lwd=2)
-arrows(0,0,3.5,1.75,lwd=3)
-text(3.5,1.75,"{\\Large ${\\bm S}$}",pos=4)
-text(2,1,"{\\huge $\\mathfrak{s}$}",pos=1,offset = 1.3)
-#lines(c(-0.25,1),c(-1,4),lwd=2)
-arrows(0,0,1,4,lwd=3)
-text(1,4,"{\\Large ${\\bm R}$}",pos=3)
-
-lines(c(0,2),c(-3,5),lty=2)
-points(1.5,3,pch=20,cex=2,col='blue')
-text(1.5,3,"{\\huge $\\color{blue}{\\hat{\\bm{y}}}$}",pos = 3,offset = 1.5)
-
-text(1.25,2,"{\\huge ${\\color{blue} s\\circ g}$}",pos=4)
-
-points(6/7,3/7,pch=20,cex=2,col='red')
-text(6/7,3/7,"{\\huge $\\color{red}{\\tilde{\\bm{y}}}$}",pos=1,offset = 1.5)
-
-arrows(1.5,3,6/7,3/7,col='blue')
-
-dev.off()
+# tikz('pointforerec_schematic.tex',height=5)
+# plot.new()
+# plot.window(xlim = c(-0.25,4),ylim = c(-0.5,5))
+# lines(c(0,0),c(-1,5))
+# lines(c(-1,5),c(0,0))
+# #lines(c(-1,3.5),c(-0.5,1.75),lwd=2)
+# arrows(0,0,3.5,1.75,lwd=3)
+# text(3.5,1.75,"{\\Large ${\\bm S}$}",pos=4)
+# text(2,1,"{\\huge $\\mathfrak{s}$}",pos=1,offset = 1.3)
+# #lines(c(-0.25,1),c(-1,4),lwd=2)
+# arrows(0,0,1,4,lwd=3)
+# text(1,4,"{\\Large ${\\bm R}$}",pos=3)
+# 
+# lines(c(0,2),c(-3,5),lty=2)
+# points(1.5,3,pch=20,cex=2,col='blue')
+# text(1.5,3,"{\\huge $\\color{blue}{\\hat{\\bm{y}}}$}",pos = 3,offset = 1.5)
+# 
+# text(1.25,2,"{\\huge ${\\color{blue} s\\circ g}$}",pos=4)
+# 
+# points(6/7,3/7,pch=20,cex=2,col='red')
+# text(6/7,3/7,"{\\huge $\\color{red}{\\tilde{\\bm{y}}}$}",pos=1,offset = 1.5)
+# 
+# arrows(1.5,3,6/7,3/7,col='blue')
+# 
+# dev.off()
 
 
 
@@ -149,5 +149,4 @@ text(3,3,"{\\huge $\\color{blue}{g^{-1}(\\mathcal{B}})$}",col='blue',pos = 3,off
 arrows(3,3,1.5,2,col='blue')
 
 dev.off()
-tools::texi2dvi('probforerec_schematic_new.tex',pdf=T)
-
+tools::texi2dvi('probforerec_schematic.tex',pdf=T,clean=T)
