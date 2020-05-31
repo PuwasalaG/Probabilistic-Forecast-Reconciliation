74f8a4b0a2030118b30063bf311ffdb49bd9079b
Anastasios Panagiotelis <anastasios.panagiotelis@monash.edu>
2020-05-31 21:20:07 +1000

    updated evaluation for scenario 28

--- Simulation/Evaluate/evaluate.R
+++ Simulation/Evaluate/evaluate.R
@@ -189,7 +189,7 @@ evaluate_scenario<-function(scen){
   return(res_final)
 }
 
-complete<-c(1:3,5:7,9:11,13:15,17:19,21:23,25:27,29:30)
+complete<-c(1:3,5:7,9:11,13:15,17:19,21:23,25:31)
 
 all_results<-map_dfr(complete,evaluate_scenario)
 
